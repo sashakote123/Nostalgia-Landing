d056a2cdd167e519e664b57f4ef023f8d0699bc0
Kotikhin Alex <sapool@bk.ru>
2024-06-21 02:41:41 +0300

    add shopping cart

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './styles/common.css';
 import './styles/reset.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
 
 
